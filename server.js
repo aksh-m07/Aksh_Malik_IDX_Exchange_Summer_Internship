6cf61f6eefc16ec51cdd585e36d65604ed31037c
Aksh Malik <akshmalik.am@gmail.com>
2026-06-26 19:23:57 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const cors=require('cors');
+require('dotenv').config();
+const db =require('./db');
+const app=express();
+const PORT=process.env.PORT ||8000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', async (req,res) =>{
+    try{
+        await db.query('SELECT 1');
+        res.json({ status: 'ok', database: 'connected' });
+    }
+    catch(err){
+         res.status(500).json({ status: 'error', database: 'disconnected', message: err.message });
+
+    }
+    
+
+});
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
